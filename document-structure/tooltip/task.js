f7fc179b59a0b78ce38951e20cf884de388786c0
HermanYudin <hermanhockey1994@yandex.ru>
2023-06-22 20:28:08 +0300

    ready

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,23 +1,48 @@
-const tooltips = Array.from(document.getElementsByClassName('has-tooltip'));
-const div = document.createElement('div');
-
-div.className = 'tooltip';
-
-tooltips.forEach(element => {
-  element.addEventListener('click', event => {
-    event.preventDefault();
-
-    target = event.target;
-
-    if (target.title === div.innerText) {
-      div.classList.toggle('tooltip_active');
-      return;
+const tooltips = Array.from(document.getElementsByClassName('has-tooltip')); 
+let tooltip = document.createElement('div'); 
+tooltip.className = 'tooltip'; 
+tooltip.innerHTML = 'Tooltip'; 
+ 
+// Функция для позиционирования тултипа  
+function setPositionTooltip(elem) { 
+    let tooltipWidth = tooltip.offsetWidth; 
+    let tooltipHeight = tooltip.offsetHeight; 
+    let elemRect = elem.getBoundingClientRect(); 
+    let left = elemRect.left + (elem.offsetWidth / 2) - (tooltipWidth / 2); 
+    let top = elemRect.top - tooltipHeight - 5; 
+ 
+    if (left < 0) left = 0; // Проверка, чтобы тултип не выходил за границы экрана слева  
+    if (left + tooltipWidth > document.documentElement.clientWidth) left = document.documentElement.clientWidth - tooltipWidth;  
+    // Проверка, чтобы тултип не выходил за границы экрана справа  
+    tooltip.style.left = left + 'px'; 
+    tooltip.style.top = top + 'px'; 
+} 
+ 
+tooltips.forEach(elem => { 
+    elem.addEventListener('click', event => { 
+        event.preventDefault(); 
+        let target = event.target; 
+ 
+        if (target.title === tooltip.innerText) { 
+            tooltip.classList.toggle('tooltip_active'); 
+            return; 
+        } 
+ 
+        tooltip.innerText = target.title; 
+        target.insertAdjacentElement('afterEnd', tooltip); 
+        setPositionTooltip(elem); 
+        tooltip.classList.add('tooltip_active'); 
+    }); 
+}); 
+ 
+document.addEventListener('mousemove', event => { 
+    if (tooltip.classList.contains('tooltip_active')) { 
+        setPositionTooltip(tooltip.previousSibling); 
     } 
-    
-    div.innerText = target.title;
-    const { bottom, left } = target.getBoundingClientRect();
-    div.style = `left: ${left}px; top: ${bottom}px`;
-    target.insertAdjacentElement('afterEnd', div);
-    div.classList.add('tooltip_active');
-  });
+}); 
+ 
+document.addEventListener('scroll', event => { 
+    if (tooltip.classList.contains('tooltip_active')) { 
+        setPositionTooltip(tooltip.previousSibling); 
+    } 
 });
